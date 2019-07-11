58fc65f91ba5882acb31a8fb8de073f571e977df
Wilfred Ruck <wilfredoruck@gmail.com>
2019-07-11 19:13:10 -0400

    generate an image from api

--- src/App.js
+++ src/App.js
@@ -5,7 +5,32 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      text: "",
       roboImage: "https://designshack.net/wp-content/uploads/placeholder-image.png",
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  updateText(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.text === "") return;
+    this.generateImage();
+  }
+
+  async generateImage() {
+    try {
+      const response = await fetch(`https://robohash.org/${this.state.text}`);
+      this.setState({ roboImage: response.url });
+    } 
+    catch {
+      alert("Couldn't generate an image for input");
     }
   }
 
@@ -14,12 +39,12 @@ class App extends React.Component {
       <div className="app">
         <h1>Wilfred Ruck</h1>
         <div className="main-content">
-          <form>
-            <input type="text" placeholder="Enter Text"/>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="Enter Text" onChange={this.updateText('text')}/>
             <input type="submit" value="Generate"/>
           </form>
 
-          <img className="image-section" src={this.state.roboImage}></img>
+          <img className="image-section" src={this.state.roboImage} alt="Robohash"></img>
         </div>
       </div>
     );
