2fd06ce63b97c473529387f6f271759b08daba62
Wilfred Ruck <wilfredoruck@gmail.com>
2019-07-11 18:40:59 -0400

    complete form design

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import './App.css';
 
 class App extends React.Component {
   constructor(props) {
-    super(props);    
+    super(props);
+    this.state = {
+      roboImage: "https://designshack.net/wp-content/uploads/placeholder-image.png",
+    }
   }
 
   render() {
     return (
       <div className="app">
         <h1>Wilfred Ruck</h1>
+        <div className="main-content">
+          <form>
+            <input type="text" placeholder="Enter Text"/>
+            <input type="submit" value="Generate"/>
+          </form>
+
+          <img className="image-section" src={this.state.roboImage}></img>
+        </div>
       </div>
     );
   }
