2052854ea6698ddbe305385a1d913f226ab2edeb
Wilfred Ruck <wilfredoruck@gmail.com>
2019-07-12 00:42:25 -0400

    refactor code and add spinner

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      loading: false,
       text: "",
       roboImage: "https://designshack.net/wp-content/uploads/placeholder-image.png",
     }
-
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -21,13 +23,14 @@ class App extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
     if (this.state.text === "") return;
+    this.setState({ loading: true });
     this.generateImage();
   }
 
   async generateImage() {
     try {
       const response = await fetch(`https://robohash.org/${this.state.text}`);
-      this.setState({ roboImage: response.url });
+      this.setState({ loading: false, roboImage: response.url });
     } 
     catch {
       alert("Couldn't generate an image for input");
@@ -35,16 +38,20 @@ class App extends React.Component {
   }
 
   render() {
+    const {loading} = this.state;
     return (
       <div className="app">
         <h1>Wilfred Ruck</h1>
         <div className="main-content">
           <form onSubmit={this.handleSubmit}>
             <input type="text" placeholder="Enter Text" onChange={this.updateText('text')}/>
-            <input type="submit" value="Generate"/>
+            <input type="submit" value="Generate" disabled={loading}/>
           </form>
 
-          <img className="image-section" src={this.state.roboImage} alt="Robohash"></img>
+          {this.state.loading ? 
+          <div className="loading-section"> <FontAwesomeIcon icon={faSpinner} /> </div>
+          : <img src={this.state.roboImage} alt="Robohash"></img>
+          }
         </div>
       </div>
     );
