db2a527bb928e1ac8d74ab4295aebac4d7b3b41e
Wilfred Ruck <wilfredoruck@gmail.com>
2019-07-20 12:43:28 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -9,18 +9,12 @@ class App extends React.Component {
     this.state = {
       loading: false,
       roboImage: "https://designshack.net/wp-content/uploads/placeholder-image.png",
-<<<<<<< HEAD
-=======
       prevInput: "",
->>>>>>> updates
       images: [],
     }
     this.textInput = React.createRef();
     this.handleSubmit = this.handleSubmit.bind(this);
-<<<<<<< HEAD
-=======
     this.focusOnInput = this.focusOnInput.bind(this);
->>>>>>> updates
     this.handleClick = this.handleClick.bind(this);
   }
 
@@ -44,23 +38,11 @@ class App extends React.Component {
     if (this.state.roboImage !== img) this.setState({roboImage: img});
   }
 
-<<<<<<< HEAD
-  handleClick(img) {
-    if (this.state.roboImage !== img) this.setState({ roboImage: img });
-  }
-
-  async generateImage() {
-    try {
-      const response = await fetch(`https://robohash.org/${this.state.text}`);
-      const newImages = this.state.images.concat(response.url);
-      this.setState({ loading: false, roboImage: response.url, images: newImages });
-=======
   async generateImage(val) {
     try {
       const response = await fetch(`https://robohash.org/${val}`);
       const imageArr = this.state.images.concat(response.url);
       this.setState({ loading: false, roboImage: response.url, prevInput: val, images: imageArr });
->>>>>>> updates
     } 
     catch {
       alert("Couldn't generate an image for input");
@@ -70,13 +52,8 @@ class App extends React.Component {
   render() {
     const {loading} = this.state;
     const images = this.state.images.map((image, idx) => {
-<<<<<<< HEAD
-      return <li key={idx} onClick={() => this.handleClick(image)}> <img className="prev-image" src={image}></img> </li>
-    });
-=======
       return <li key={idx} onClick={() => this.handleClick(image)}> <img src={image} className="prev-images" alt="Previous Robots"></img> </li>
     })
->>>>>>> updates
     return (
       <div className="app">
         <h1>Wilfred Ruck</h1>
